04437f618e36ac4e4b7159cfc4bbee21e98e9487
CalmaMente <143668178+CalmaMente@users.noreply.github.com>
2024-06-19 15:49:13 -0300

    ad componets to src

---
+++ src/components/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './../../../components/dashboard/dashboard/dashboard.component.html',
+  styleUrls: ['./../../../components/dashboard/dashboard/dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
