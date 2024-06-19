0e4c24ffb8a75ae3ca7d43d037d50528f61c5a90
CalmaMente <143668178+CalmaMente@users.noreply.github.com>
2024-06-19 15:39:57 -0300

    ad compoentes to src

---
+++ src/app/components/dashboard/home-dashboard/home-dashboard.component.ts
@@ -0,0 +1,14 @@
+// INSERIDO POR LAURA
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-dashboard',
+  templateUrl: './home-dashboard.component.html',
+  styleUrls: ['./home-dashboard.component.css']
+})
+export class HomeDashboardComponent implements OnInit{
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
